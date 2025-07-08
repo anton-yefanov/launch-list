198bb947ac6328c850e3310ec9adae5e7ac5759f
antonyefanov <antonyefanov@gmail.com>
2025-07-08 21:12:55 +0300

    save

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,7 +1,7 @@
 import { Resend } from "resend";
 import { EmailTemplate } from "@/components/email-template";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.AUTH_RESEND_KEY);
 
 export async function POST() {
   try {
