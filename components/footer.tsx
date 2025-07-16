bb8382f0605702a4dfc79c5fb8ff0f77403135ff
Anton Yefanov <xt.yefonova@gmail.com>
2025-07-16 10:19:24 +0300

    save

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,16 +2,38 @@ import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <div className="text-sm flex gap-4 mt-5 justify-center">
-      <Link href="/tos" className="hover:underline">
-        Terms of Service
-      </Link>
-      <Link href="/privacy" className="hover:underline">
-        Privacy policy
-      </Link>
-      <Link href="/blog" className="hover:underline">
-        Blog
-      </Link>
+    <div className="mt-8 flex flex-col justify-center items-center gap-4">
+      <div className="text-sm flex gap-4  justify-center">
+        <Link href="/tos" className="hover:underline">
+          Terms of Service
+        </Link>
+        <Link href="/privacy" className="hover:underline">
+          Privacy policy
+        </Link>
+        <Link href="/blog" className="hover:underline">
+          Blog
+        </Link>
+      </div>
+      <div className="flex items-center gap-2">
+        <a href="https://www.uneed.best/tool/launch-list">
+          <img
+            src="https://www.uneed.best/POTD1.png"
+            alt="Uneed POTD1 Badge"
+            className="w-38"
+          />
+        </a>
+        <a
+          href="https://startupfa.me/s/launch-list?utm_source=www.launch-list.org"
+          target="_blank"
+        >
+          <img
+            src="https://startupfa.me/badges/featured-badge-small.webp"
+            alt="Featured on Startup Fame"
+            width="224"
+            height="36"
+          />
+        </a>
+      </div>
     </div>
   );
 };
