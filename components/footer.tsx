4d6f40694597b2138bd9c0887c2732eec8f41914
antonyefanov <antonyefanov@gmail.com>
2025-07-26 23:31:55 +0300

    save

--- components/footer.tsx
+++ components/footer.tsx
@@ -21,6 +21,15 @@ export const Footer = () => {
             alt="Fazier badge"
           />
         </a>
+        <a href="https://yo.directory/" target="_blank">
+          <img
+            src="https://cdn.prod.website-files.com/65c1546fa73ea974db789e3d/65e1e171f89ebfa7bd0129ac_yodirectory-featured.png"
+            alt="yo.directory"
+            style={{ width: 150, height: 54 }}
+            width="150"
+            height="54"
+          />
+        </a>
       </div>
       <div className="text-sm flex gap-4  justify-center">
         <Link href="/blog" className="text-gray-600 hover:underline">
